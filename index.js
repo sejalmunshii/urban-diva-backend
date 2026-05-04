4f71ffbfdadd6feb5062611b4c038018a21bfce9
sejalmunshii <sejalmunshi11@gmail.com>
2026-05-04 14:02:24 +0530

    Fix index.js order

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(cors({
   origin: [
     "http://localhost:3000",
@@ -17,12 +19,14 @@ app.use(cors({
   ],
   credentials: true
 }));
+
 app.use(express.json());
 
 // Routes
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Urban Diva API Running! 🌸");
@@ -32,5 +36,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-const userRoutes = require("./routes/userRoutes");
-app.use("/api/users", userRoutes);
